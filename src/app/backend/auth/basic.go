dc091240c0890fdb67bb4fb72be3dbcad9690053
skmdab <57156958+skmdab@users.noreply.github.com>
2024-01-21 11:52:33 +0530

    restored as it before

--- src/app/backend/auth/basic.go
+++ src/app/backend/auth/basic.go
@@ -12,34 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// basic.go
-
 package auth
 
 import (
-    authApi "github.com/kubernetes/dashboard/src/app/backend/auth/api"
-    "k8s.io/client-go/tools/clientcmd/api"
+	authApi "github.com/kubernetes/dashboard/src/app/backend/auth/api"
+	"k8s.io/client-go/tools/clientcmd/api"
 )
 
 // Implements Authenticator interface
 type basicAuthenticator struct {
-    username string
-    password string
+	username string
+	password string
 }
 
 // GetAuthInfo implements Authenticator interface. See Authenticator for more information.
 func (self *basicAuthenticator) GetAuthInfo() (api.AuthInfo, error) {
-    return api.AuthInfo{
-        Username: self.username,
-        Password: self.password,
-    }, nil
+	return api.AuthInfo{
+		Username: self.username,
+		Password: self.password,
+	}, nil
 }
 
 // NewBasicAuthenticator returns Authenticator based on LoginSpec.
 func NewBasicAuthenticator(spec *authApi.LoginSpec) authApi.Authenticator {
-    return &basicAuthenticator{
-        username: spec.Username,
-        password: spec.Password,
-    }
+	return &basicAuthenticator{
+		username: spec.Username,
+		password: spec.Password,
+	}
 }
-
